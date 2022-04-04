bdfaa050ff02757bccd73e69fe3b4a6f2408e970
Petci <101003958+Petci@users.noreply.github.com>
2022-04-04 15:56:06 +0200

    Add files via upload

--- AsonnakTeszt.java
+++ AsonnakTeszt.java
@@ -5,7 +5,18 @@ import java.util.Random;
 
 public class AsonnakTeszt {
 
+    public static void kiir(int[][] Domb) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(Domb[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
+        //Domborzat létrehozása
         Random rnd = new Random();
         //[Sor] [Oszlop]
         int[][] Domb = new int[4][4];
@@ -39,31 +50,32 @@ public class AsonnakTeszt {
         for (int i = 1; i <= 3; i++) {
             for (int j = 0; j <= 3; j++) {
                 if (j == 0) {
-                        if (Domb[i - 1][j] == 1) {
-                            int random = rnd.nextInt(2);
-                            if (random == 0) {
-                                Domb[i][j] = Domb[i-1][j];
-                            } else {
-                                Domb[i][j] = Domb[i-1][j] + 1;
-                            }
-                        } else if (Domb[i - 1][j] == 4) {
-                            int random = rnd.nextInt(2);
-                            if (random == 0) {
-                                Domb[i][j] = Domb[i-1][j];
-                            } else {
-                                Domb[i][j] = Domb[i-1][j] - 1;
-                            }
+                    if (Domb[i - 1][j] == 1) {
+                        int random = rnd.nextInt(2);
+                        if (random == 0) {
+                            Domb[i][j] = Domb[i - 1][j];
                         } else {
-                            int random = rnd.nextInt(3);
-                            if (random == 0) {
-                                Domb[i][j] = Domb[i - 1][j] - 1;
-                            } else if (random == 1) {
-                                Domb[i][j] = Domb[i - 1][j];
-                            } else {
-                                Domb[i][j] = Domb[i - 1][j] + 1;
-                            }
+                            Domb[i][j] = Domb[i - 1][j] + 1;
                         }
-                }if (j > 0 && j < 4) {
+                    } else if (Domb[i - 1][j] == 4) {
+                        int random = rnd.nextInt(2);
+                        if (random == 0) {
+                            Domb[i][j] = Domb[i - 1][j];
+                        } else {
+                            Domb[i][j] = Domb[i - 1][j] - 1;
+                        }
+                    } else {
+                        int random = rnd.nextInt(3);
+                        if (random == 0) {
+                            Domb[i][j] = Domb[i - 1][j] - 1;
+                        } else if (random == 1) {
+                            Domb[i][j] = Domb[i - 1][j];
+                        } else {
+                            Domb[i][j] = Domb[i - 1][j] + 1;
+                        }
+                    }
+                }
+                if (j > 0 && j < 4) {
                     if (Domb[i - 1][j] == Domb[i][j - 1]) {
                         if (Domb[i - 1][j] == 1) {
                             int random = rnd.nextInt(2);
@@ -89,10 +101,10 @@ public class AsonnakTeszt {
                                 Domb[i][j] = Domb[i - 1][j] + 1;
                             }
                         }
-                    } else if (Domb[i - 1][j] != Domb[i][j - 1] && (Domb[i-1][j]-Domb[i][j-1]==2 || Domb[i][j-1]-Domb[i-1][j]==2)) {
+                    } else if (Domb[i - 1][j] != Domb[i][j - 1] && (Domb[i - 1][j] - Domb[i][j - 1] == 2 || Domb[i][j - 1] - Domb[i - 1][j] == 2)) {
                         Domb[i][j] = (Domb[i - 1][j] + Domb[i][j - 1]) / 2;
                     } else {
-                        if (Domb[i - 1][j] == 1 || Domb[i][j - 1] == 1 ) {
+                        if (Domb[i - 1][j] == 1 || Domb[i][j - 1] == 1) {
                             int random = rnd.nextInt(2);
                             if (random == 0) {
                                 Domb[i][j] = 1;
@@ -119,38 +131,146 @@ public class AsonnakTeszt {
             }
 
         }
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(Domb[i][j]);
-            }
-            System.out.println();
-        }
-        
-        
-        int search = 4;
+
+        kiir(Domb);
+
+        //Első legkisebb
+        int s = 4;
+        int ct = 0;
+
         for (int i = 0; i < Domb.length; i++) {
             for (int j = 0; j < Domb.length; j++) {
-                if (search > Domb[i][j]) {
-                    search = Domb[i][j];
+                if (s > Domb[i][j]) {
+                    s = Domb[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < Domb.length; i++) {
+            for (int j = 0; j < Domb.length; j++) {
+                if (s == Domb[i][j]) {
+                    ct++;
+                }
+            }
+        }
+
+        int r = rnd.nextInt(ct);
+        ct = -1;
+        int k1 = 0;
+        int k2 = 0;
+
+        for (int i = 0; i < Domb.length; i++) {
+            for (int j = 0; j < Domb.length; j++) {
+                if (s == Domb[i][j]) {
+                    ct++;
+                    if (ct == r) {
+                        Domb[i][j] *= -1;
+                        k1 = i;
+                        k2 = j;
+                    }
+                }
+            }
+        }
+
+        kiir(Domb);
+
+        //Terjedés és emelkedés
+        int count = 0;
+        for (int i = 0; i < Domb.length; i++) {
+            for (int j = 0; j < Domb.length; j++) {
+                if (i + 1 == k1 && j == k2) {
+                    if (Domb[k1][k2] == Domb[i][j] * -1) {
+                        Domb[i][j] = Domb[k1][k2];
+                        count++;
+                    }
+                }
+                if (i == k1 && j + 1 == k2) {
+                    if (Domb[k1][k2] == Domb[i][j] * -1) {
+                        Domb[i][j] = Domb[k1][k2];
+                        count++;
+                    }
+                }
+                if (i == k1 && j == k2 + 1) {
+                    if (Domb[k1][k2] == Domb[i][j] * -1) {
+                        Domb[i][j] = Domb[k1][k2];
+                        count++;
+                    }
+                }
+                if (i == k1 + 1 && j == k2) {
+                    if (Domb[k1][k2] == Domb[i][j] * -1) {
+                        Domb[i][j] = Domb[k1][k2];
+                        count++;
+                    }
+                }
+            }
+        }
+
+        int cnsz = 0; // Count: Nem szomszédos
+
+        do {
+            for (int e = 0; e < Domb.length; e++) {
+                for (int k = 0; k < Domb.length; k++) {
+                    if (Domb[e][k] < 0) {
+                        for (int i = 0; i < Domb.length; i++) {
+                            for (int j = 0; j < Domb.length; j++) {
+                                cnsz = 0;
+                                if (i + 1 == e && j == k && count != 4) {
+                                    if (Domb[e][k] == Domb[i][j] * -1) {
+                                        Domb[i][j] = Domb[e][k];
+                                        count++;
+                                        cnsz++;
+                                    }
+                                }
+                                if (i == e && j + 1 == k && count != 4) {
+                                    if (Domb[e][k] == Domb[i][j] * -1) {
+                                        Domb[i][j] = Domb[e][k];
+                                        count++;
+                                        cnsz++;
+                                    }
+                                }
+                                if (i == e && j == k + 1 && count != 4) {
+                                    if (Domb[e][k] == Domb[i][j] * -1) {
+                                        Domb[i][j] = Domb[e][k];
+                                        count++;
+                                        cnsz++;
+                                    }
+                                }
+                                if (i == e + 1 && j == k && count != 4) {
+                                    if (Domb[e][k] == Domb[i][j] * -1) {
+                                        Domb[i][j] = Domb[e][k];
+                                        count++;
+                                        cnsz++;
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+        } while (count != 4 && cnsz != 0);
+
+        kiir(Domb);
+        System.out.println(count);
+        
+        
+        
+        
+        
+        //Emelkedés
+        int v = Domb[k1][k2];
+        for (int i = 0; i < Domb.length; i++) {
+            for (int j = 0; j < Domb.length; j++) {
+                if (Domb[i][j]==v) {
+                    Domb[i][j] -= 1;
                 }
                 
             }
             
         }
+        kiir(Domb);
         
-        
-        System.out.println(search);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
     }
 }
